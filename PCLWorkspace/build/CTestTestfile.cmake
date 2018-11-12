d3a02ca4f88a94e0dea2e4e53c05a957513458a3
DALLAN BYRNE <u180120@CS4-10>
2018-11-12 11:18:46 +0000

    Tutorial used code

---
+++ PCLWorkspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/u180120/ProjectWork/PCLWorkspace/src
+# Build directory: /home/u180120/ProjectWork/PCLWorkspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_pcl_tutorial)
---
+++ PCLWorkspace/build/my_pcl_tutorial/CMakeFiles/icp_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp_example.dir/src/icp_example.cpp.o"
+  "/home/u180120/ProjectWork/PCLWorkspace/devel/lib/my_pcl_tutorial/icp_example.pdb"
+  "/home/u180120/ProjectWork/PCLWorkspace/devel/lib/my_pcl_tutorial/icp_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
