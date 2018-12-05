196903393dbc9e38ee07a662dcc99b04762a53e1
Dallan Byrne <simplisticdude@gmail.com>
2018-12-05 14:06:02 +0000

    ICP compare

--- PCLWorkspace/src/my_pcl_tutorial/src/icp_example.cpp
+++ PCLWorkspace/src/my_pcl_tutorial/src/icp_example.cpp
@@ -1,47 +1,55 @@
 #include <iostream>
+#include <vector>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/registration/icp.h>
+#include <pcl/filters/impl/filter.hpp>
+#include <pcl/filters/voxel_grid.h>
 
-int
- main (int argc, char** argv)
+int main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out (new pcl::PointCloud<pcl::PointXYZ>);
 
-  // Fill in the CloudIn data
-  cloud_in->width    = 5;
-  cloud_in->height   = 1;
-  cloud_in->is_dense = false;
-  cloud_in->points.resize (cloud_in->width * cloud_in->height);
-  for (size_t i = 0; i < cloud_in->points.size (); ++i)
-  {
-    cloud_in->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud_in->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud_in->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+	if(pcl::io::loadPCDFile ("/usr/local/home/u180120/nethome/ProjectWork/PCLWorkspace/src/my_pcl_tutorial/src/1539950563.428160477.pcd", *cloud_in)==-1)
+	{
+		PCL_ERROR ("Couldn't read first file! \n");
+		return (-1);
+	}
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cleaned_cloud_in (new pcl::PointCloud<pcl::PointXYZ>);
+
+  std::vector<int> index;
+
+  pcl::removeNaNFromPointCloud(*cloud_in, *cleaned_cloud_in, index);
+  pcl::io::savePCDFileASCII ("cleaned.pcd", *cleaned_cloud_in);
+
+  *cloud_out = *cleaned_cloud_in;
+
+	std::cout << "cloud_in size: " << cloud_in->points.size() << std::endl;
+  std::cout << "cleaned_cloud_in size: " <<cleaned_cloud_in->points.size() << std::endl;
+  std::cout << "cloud_out size: " << cloud_out ->points.size() << std::endl;
+
+  for(size_t i = 0; i < cloud_out->points.size(); ++i){
+    cloud_out->points[i].x = cloud_out->points[i].x + 0.005f;
   }
-  std::cout << "Saved " << cloud_in->points.size () << " data points to input:"
-      << std::endl;
-  for (size_t i = 0; i < cloud_in->points.size (); ++i) std::cout << "    " <<
-      cloud_in->points[i].x << " " << cloud_in->points[i].y << " " <<
-      cloud_in->points[i].z << std::endl;
-  *cloud_out = *cloud_in;
-  std::cout << "size:" << cloud_out->points.size() << std::endl;
-  for (size_t i = 0; i < cloud_in->points.size (); ++i)
-    cloud_out->points[i].x = cloud_in->points[i].x + 0.7f;
-  std::cout << "Transformed " << cloud_in->points.size () << " data points:"
-      << std::endl;
-  for (size_t i = 0; i < cloud_out->points.size (); ++i)
-    std::cout << "    " << cloud_out->points[i].x << " " <<
-      cloud_out->points[i].y << " " << cloud_out->points[i].z << std::endl;
-  pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-  icp.setInputSource(cloud_in);
-  icp.setInputTarget(cloud_out);
-  pcl::PointCloud<pcl::PointXYZ> Final;
-  icp.align(Final);
-  std::cout << "has converged:" << icp.hasConverged() << " score: " <<
-  icp.getFitnessScore() << std::endl;
-  std::cout << icp.getFinalTransformation() << std::endl;
+       
+  pcl::io::savePCDFileASCII ("output.pcd", *cloud_out);
+  std::cout << "Transformed " << cleaned_cloud_in->points.size() << " data points:" << std::endl;
 
- return (0);
+
+ 	pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;       
+	icp.setMaximumIterations(10);
+	icp.setMaxCorrespondenceDistance(0.01);
+	icp.setInputSource(cleaned_cloud_in);
+	icp.setInputTarget(cloud_out);
+	pcl::PointCloud<pcl::PointXYZ> Final;
+	Eigen::Matrix4f transformation;
+	icp.align(Final, transformation);
+
+	std::cout << "has converged:" << icp.hasConverged() << " score: " << icp.getFitnessScore() << std::endl;
+	
+	std::cout << "Transformation: " << transformation << std::endl;
+
+return (0);
 }
